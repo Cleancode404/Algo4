b7dceb28af730baec16c37fd77cc8ef40e622d82
Heuristic <codemonkey2468@hotmail.com>
2020-10-17 09:59:16 -0400

    update chapter 1

---
+++ chapter1/Demo/java_statements.java
@@ -0,0 +1,53 @@
+//java statements
+
+//declaration 
+int i;
+double c;
+
+//assignment 
+a = b + 3;
+discrminant = b*b -4.0*c;
+
+//initializing declaration 
+int i = 1;
+double c = 3.141592625;
+
+//implicit 
+i++;
+i +=1;
+
+//conditional (if). If a block of statements in a conditional or a loop has only 
+// One signle statement, the curly braces may be omitted
+if (x<0)
+	x = -x;
+
+//conditional (if-else)
+if (x > y){
+	max = x;
+}
+else{
+	max = y;
+}
+
+
+//while loop
+int power = 1;
+while (power <= n)
+	power *=2;
+
+//for loop
+for (int i = 1; i <= n; i++)
+	sum +=1.0/i;
+
+
+//call
+int key = StdIn.readInt();
+
+
+//return 
+return false;
+
+
+
+
+
