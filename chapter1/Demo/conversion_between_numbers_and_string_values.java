4758e16b9bad154a0d9c86adbf81ddd00c0016da
Heuristic <codemonkey2468@hotmail.com>
2021-01-21 09:53:46 -0500

    conversion between string and string

---
+++ chapter1/Demo/conversion_between_numbers_and_string_values.java
@@ -0,0 +1,12 @@
+// convert S to an integer value
+static int parseInt(String S)
+
+//convert i to a string value
+static String toString(int i)
+
+
+//convert S to double value
+static double parseDouble(String s)
+
+//convert x to a string value
+static String toString(double x)
