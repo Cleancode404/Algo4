391b61e2659c8e734035db5510060a60f9f743f8
Heuristic <codemonkey2468@hotmail.com>
2021-01-22 16:03:15 -0500

    Exercise_1_1_13

---
+++ chapter1/Exercise/Exercise_1_1_13.java
@@ -0,0 +1,9 @@
+/**Write a code fragment to print the transposition (rows and columns changed)
+of a two-dimensional array with M rows and N columns. */
+
+public class Exercise_1_1_13 {
+    public static void main(String[] args){
+        
+    }
+    
+}
