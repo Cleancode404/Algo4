f72258452948e1915f36f8dcb3dc5cbe791e3274
Heuristic <codemonkey2468@hotmail.com>
2021-01-22 15:40:14 -0500

    Exercise_1_1_7c

---
+++ chapter1/Exercise/Exercise_1_1_7c.java
@@ -0,0 +1,14 @@
+/**exercise 1.1.7 c**/
+
+public class Exercise_1_1_7c{
+    public static void main(String[] args){
+        int sum = 0;
+        for (int i= 1; i < 10; i *= 2){
+            for(int j = 0; j < 1000; j++){
+                sum++;
+        System.out.println(sum);
+            }
+
+        }
+    }
+}
